5e2831610b73d7c563e0576e76e58df7fa820876
Don Aurelio <aurelio.vivas@correounivalle.edu.co>
2015-04-15 22:59:30 -0500

    Clase Reporte

---
+++ Clases/Reporte.py
@@ -0,0 +1,41 @@
+class Reporte(Observable):
+
+	def getCursoMayorNumeroAsistentes(self,periodo,tipo):
+		if tipo == "mes":
+			pass
+		elif tipo == "semestre":
+			pass
+
+	def getNumeroLeaderTeachersNuevosPorDepartamento(self,periodo,tipo):
+		if tipo == "mes":
+			pass
+		elif tipo == "semestre":
+			pass
+
+	def getCursosConMenorPotenciaAvance(self,periodo,tipo):
+		if tipo == "mes":
+			pass
+		elif tipo == "semestre":
+			pass
+
+	def getPorcentajeLeaderTeacherAprobaronCursos(self,periodo,tipo,departamentos):
+		if tipo == "mes":
+			pass
+		elif tipo == "semestre":
+			pass
+
+	def getPorcentajeLeaderTeacherReporbaronCursos(self,periodo,tipo,departamentos):
+		if tipo == "mes":
+			pass
+		elif tipo == "semestre":
+			pass
+	def getDetalleReportesNotasPorEstudiante(self,mes):
+		pass
+
+	def getDetalleEstudiantesEnCursos(self,mes,departamentos):
+		pass
+
+	def update():
+		pass
+
+			
