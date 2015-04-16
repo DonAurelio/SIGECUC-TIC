2acea9698baf8baad37fd2b518e2a786b35d802b
EduardoSP <eduardo.saavedra@correounivalle.edu.co>
2015-04-15 23:32:22 -0500

    clases MasterTeacher y Persona

---
+++ Clases/MasterTeacher.py
@@ -0,0 +1,21 @@
+from Persona import Persona
+
+
+class MasterTecaher(Persona):
+    def __init__(self, identificacion, nombres, apellidos, email, telefono,
+    direccion, estado_civil, contrasenia):
+        # llamamos al constructor de Persona
+        Persona.__init__(self, identificacion, nombres, apellidos, email,
+             telefono, direccion, estado_civil)
+        # agregamos el nuevo atributo
+        self.contrasenia = contrasenia
+
+    def getContrasenia(self):
+        return self.contrasenia
+
+    def setContrasenia(self, contrasenia):
+        self.contrasenia = contrasenia
+
+
+
+
