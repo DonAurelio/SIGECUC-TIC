e9e28854e90dbe0ff6ed527ed206cca505d2a532
Don Aurelio <aurelio.vivas@correounivalle.edu.co>
2015-04-16 23:53:14 -0500

    Registro de modelos en el site del administrador

--- SIGECUC_TIC/aplicacion_cursos_tic/admin.py
+++ SIGECUC_TIC/aplicacion_cursos_tic/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from models import Persona
+#Administrador no es necesario
+#from models import Administrador
+from models import MasterTeacher
+from models import HistorialLaboral
+from models import HistorialAcademico
+from models import Inscrito
+from models import LeaderTeacher
+from models import AreaFormacion
+from models import ActividadEvaluacion
+from models import Curso
+from models import Cohorte
+from models import Calificacion
+from models import LeaderTeacher_Cohorte
+
+#REGISTRO DE MODELOS EN EL SITIO DE ADMINISTRACION
+#DEL ADMINISTRADOR
 
 # Register your models here.
+admin.site.register(Persona)
+#Administrador no es necesario
+#admin.site.register(Administrador)
+admin.site.register(MasterTeacher)
+admin.site.register(HistorialLaboral)
+admin.site.register(Inscrito)
+admin.site.register(LeaderTeacher)
+admin.site.register(AreaFormacion)
+admin.site.register(ActividadEvaluacion)
+admin.site.register(Curso)
+admin.site.register(Cohorte)
+admin.site.register(Calificacion)
+admin.site.register(LeaderTeacher_Cohorte)
+
+
--- SIGECUC_TIC/aplicacion_cursos_tic/models.py
+++ SIGECUC_TIC/aplicacion_cursos_tic/models.py
@@ -13,16 +13,14 @@ class Persona(models.Model):
     direccion = models.CharField(max_length=40)
     estado_civil = models.CharField(max_length=20)
 
-
 class Administrador(models.Model):
     contrasenia = models.CharField(max_length=40)
     persona = models.OneToOneField(Persona, primary_key=True)
 
-
+    
 class MasterTeacher(models.Model):
     contrasenia = models.CharField(max_length=40)
     persona = models.OneToOneField(Persona, primary_key=True)
-
 
 class HistorialLaboral(models.Model):
     exp_preescolar = models.CharField(max_length=50)
@@ -36,47 +34,41 @@ class HistorialLaboral(models.Model):
     exp_privado = models.CharField(max_length=50)
     exp_total = models.CharField(max_length=50)
 
-
 class HistorialAcademico(models.Model):
     zona_labor_docente = models.CharField(max_length=50)
     caracter_educacion_media = models.CharField(max_length=50)
     etnia_educativa = models.CharField(max_length=50)
     nivel_educativo = models.CharField(max_length=50)
 
-
 class Inscrito(models.Model):
- 	persona = models.OneToOneField(Persona, primary_key=True)
-	fecha_inscripcion = models.DateField(auto_now_add=True)
- 	estado = models.BooleanField(default=True)
- 	historial_laboral = models.OneToOneField(HistorialLaboral)  # delete cascade
- 	historial_academico = models.OneToOneField(HistorialAcademico)
-	 
+    persona = models.OneToOneField(Persona, primary_key=True)
+    fecha_inscripcion = models.DateField(auto_now_add=True)
+    estado = models.BooleanField(default=True)
+    historial_laboral = models.OneToOneField(HistorialLaboral)  # delete cascade
+    historial_academico = models.OneToOneField(HistorialAcademico)
 
 class LeaderTeacher(models.Model):
-	 persona = models.OneToOneField(Persona, primary_key=True)
-	 contrasenia = models.CharField(max_length=40)
-	 #permiso = models.
-	 #viatico = models.
-	 fecha_nacimiento = models.DateField()
-	 sexo = models.CharField(max_length=1)  # {M, F}
-	 ciudad_nacimiento = models.CharField(max_length=30)
-	 pais_nacimiento = models.CharField(max_length=30)
-	 ciudad_residencia = models.CharField(max_length=30)
-	 pais_residencia = models.CharField(max_length=30)
-	 ciudad_labora = models.CharField(max_length=30)
-	 departamento_labora = models.CharField(max_length=30)
-	 fecha_activacion = models.DateField()
-
+    persona = models.OneToOneField(Persona, primary_key=True)
+    contrasenia = models.CharField(max_length=40)
+    #permiso = models.
+    #viatico = models.
+    fecha_nacimiento = models.DateField()
+    sexo = models.CharField(max_length=1)  # {M, F}
+    ciudad_nacimiento = models.CharField(max_length=30)
+    pais_nacimiento = models.CharField(max_length=30)
+    ciudad_residencia = models.CharField(max_length=30)
+    pais_residencia = models.CharField(max_length=30)
+    ciudad_labora = models.CharField(max_length=30)
+    departamento_labora = models.CharField(max_length=30)
+    fecha_activacion = models.DateField()
 
 class AreaFormacion(models.Model):
-	nombre = models.CharField(max_length=30)
-	descripcion = models.TextField()
-
+    nombre = models.CharField(max_length=30)
+    descripcion = models.TextField()
 
 class ActividadEvaluacion(models.Model):
     descripcion = models.TextField()
     peso = models.DecimalField(max_digits=3, decimal_places=2)  # ej: 0.75
-
 
 class Curso(models.Model):
     nombre = models.CharField(max_length=50)
@@ -85,13 +77,11 @@ class Curso(models.Model):
     area_formacion = models.ManyToManyField(AreaFormacion)
     actividad_evaluacion = models.ManyToManyField(ActividadEvaluacion)
 
-
 class Cohorte(models.Model):
     fecha_inicio = models.DateField()
     fecha_fin = models.DateField()
     curso = models.ForeignKey(Curso)
     master_teacher = models.ForeignKey(MasterTeacher)
-
 
 class Calificacion(models.Model):
     nota_actividad = models.DecimalField(max_digits=3, decimal_places=2)  # 4.25
@@ -99,8 +89,8 @@ class Calificacion(models.Model):
     cohorte = models.ForeignKey(Cohorte)
     actividad = models.ForeignKey(ActividadEvaluacion)
 
-
 class LeaderTeacher_Cohorte(models.Model):
     nota_final = models.DecimalField(max_digits=3, decimal_places=2)
     leader_teacher = models.ForeignKey(LeaderTeacher)
     cohorte = models.ForeignKey(Cohorte)
+
