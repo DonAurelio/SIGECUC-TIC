2fbfbc8f9173c7fdafe79db0d577c144eb7949df
Don Aurelio <aurelio.vivas@correounivalle.edu.co>
2015-04-21 22:40:14 -0500

    Sin modificaciones exitosas

--- SIGECUC_TIC/SIGECUC_TIC/urls.py
+++ SIGECUC_TIC/SIGECUC_TIC/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from aplicacion_cursos_tic.views import Login
-
+from aplicacion_cursos_tic.views import inicio
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,7 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/$', Login.as_view(), name='login'),
-    #url(r'^login/$', 'aplicacion_cursos_tic.views.login_page'),
-    #url(r'^login/$','views.login_page', name="login"),
+   
 )
