03ea6fc7a99de16292d816808ee02f34ce4f32ec
EduardoSP <eduardo.saavedra@correounivalle.edu.co>
2015-04-24 23:08:51 -0500

    cambios url views.py agregando inscripcion.html y listarcursos.html

--- SIGECUC_TIC/SIGECUC_TIC/urls.py
+++ SIGECUC_TIC/SIGECUC_TIC/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from aplicacion_cursos_tic.views import login
 from aplicacion_cursos_tic.views import index
+from aplicacion_cursos_tic.views import listar_cursos
+from aplicacion_cursos_tic.views import send_course
+from aplicacion_cursos_tic.views import inscripcion
+from aplicacion_cursos_tic.views import registre
 
 
 
@@ -11,8 +15,11 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login',login),
+    url(r'^login', login),
     url(r'^index', index),
-   
-   
+    url(r'^listar_cursos', listar_cursos),
+    url(r'^course/$', send_course),
+    url(r'^inscripcion/$', inscripcion),
+    url(r'^registre/$', registre)
+
 )
--- SIGECUC_TIC/aplicacion_cursos_tic/views.py
+++ SIGECUC_TIC/aplicacion_cursos_tic/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, render_to_response
 from django.contrib.auth import authenticate, login
 from django.views.generic import TemplateView
 from forms import LoginForm
+from .models import Curso
 
 from django.http import HttpResponse
 
@@ -11,8 +12,49 @@ from django.http import HttpResponse
 def login(request):
 	return render_to_response('login.html')
 
+
 def index(request):
 	return render_to_response('index.html')
 
+#==============================================================================
+def listar_cursos(request):
+    #funcion que lista los cursos activos cuando el estado es 1
+	cursos = Curso.objects.filter(estado='1')
+	return render(request, 'listar_cursos.html', { 'cursos': cursos })
 
+
+def send_course(request):
+    id_course = request.GET.get('id_course')
+    name_course = request.GET.get('name_course')
+    return render(request, 'inscripcion.html', {'id_course': id_course, 'name_course': name_course})
+    #html = "<html><body><h1>id course:</h1><h3>%s<h/3> <h1> Name curso</h1><h2>%s<h/2></body></html>" % (id_course, name_course)
+    #return HttpResponse(html)
+
+
+def inscripcion(request):
+    return render_to_response('inscripcion.html')
+
+
+def get_checkBox(request, count, name):
+    i = 1
+    string_check = ''
+    while i<=count:
+        if request.POST.get and name + str(i) in request.POST and request.POST[name + str(i)] == "on":
+            string_check = string_check + request.POST[name + str(i)]
+            i = i + 1
+        else:
+            i = i + 1
+    return string_check
+
+def registre(request):
+    errors = []
+    if request.method == 'POST':
+        if not request.POST.get('identificacion', ''):
+            errors.append('Por favor introduce la identificacion.')
+        if not errors:
+            values = [request.POST['identificacion'], request.POST['date_birth'],request.POST['sexo'],request.POST['caracter']]
+            zonas = get_checkBox(request, 4, 'zona')
+            html = "<html><body><h1></h1><h3>conas %s</h3> </body></html>" % zonas
+            return HttpResponse(html)
+    return render(request, 'inscripcion.html', {'errors': errors,'identificacion': request.POST.get('identificacion', ''),})
 
