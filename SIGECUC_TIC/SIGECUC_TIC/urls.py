871c1dfac891bc0b06b6c8a4f49a629dacc8056b
Don Aurelio <aurelio.vivas@correounivalle.edu.co>
2015-04-27 22:31:36 -0500

    cambios

--- SIGECUC_TIC/SIGECUC_TIC/urls.py
+++ SIGECUC_TIC/SIGECUC_TIC/urls.py
@@ -1,41 +1,21 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-<<<<<<< HEAD
-from aplicacion_cursos_tic.views import login
-from aplicacion_cursos_tic.views import index
-from aplicacion_cursos_tic.views import listar_cursos
-from aplicacion_cursos_tic.views import send_course
-from aplicacion_cursos_tic.views import inscripcion
-from aplicacion_cursos_tic.views import registre
-=======
-from aplicacion_cursos_tic.views import login_page_view
+
 from aplicacion_cursos_tic.views import index_page_view
-from aplicacion_cursos_tic.views import contactenos_page_view
-from aplicacion_cursos_tic.views import quienes_somos_page_view
->>>>>>> 90e1ddf447dc5333ee9ee09b9233c87c8eebb01a
-
-
+from aplicacion_cursos_tic.views import login_page_view
+from aplicacion_cursos_tic.views import contact_page_view
+from aplicacion_cursos_tic.views import information_page_view
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'SIGECUC_TIC.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-<<<<<<< HEAD
-    url(r'^login', login),
-    url(r'^index', index),
-    url(r'^listar_cursos', listar_cursos),
-    url(r'^course/$', send_course),
-    url(r'^inscripcion/$', inscripcion),
-    url(r'^registre/$', registre)
+    url(r'^admin/$', include(admin.site.urls)),
+    url(r'^index', index_page_view, name='index'),
+    url(r'^login.html', login_page_view, name='login'),
+    url(r'^contact$', contact_page_view, name='contact'),
+    url(r'^information$', information_page_view, name='information'),
 
-=======
-    url(r'^login/',login_page_view),
-    url(r'^index/', index_page_view),
-    url(r'^contactenos/',contactenos_page_view),
-    url(r'^quienes_somos/',quienes_somos_page_view)
-   
-   
->>>>>>> 90e1ddf447dc5333ee9ee09b9233c87c8eebb01a
-)
+      
+    )
--- SIGECUC_TIC/aplicacion_cursos_tic/views.py
+++ SIGECUC_TIC/aplicacion_cursos_tic/views.py
@@ -3,15 +3,16 @@ from django.shortcuts import render, render_to_response
 from django.contrib.auth import authenticate, login
 from django.views.generic import TemplateView
 from forms import LoginForm
-from .models import Curso
+
 
 from django.http import HttpResponse
 
 # Create your views here.
 
-<<<<<<< HEAD
+def index_page_view(request):
+	return render_to_response('index.html')
+
 def login_page_view(request):
-	
 	message = None
 	if request.method == "POST":
 		form = LoginForm(request.POST)
@@ -34,43 +35,8 @@ def login_page_view(request):
 	return render_to_response('login.html',{'message':message,'form':form}, context_instance=RequestContext(request))
 	
 
-def index_page_view(request):
-=======
-def login(request):
-	return render_to_response('login.html')
+def contact_page_view(request):
+	return render_to_response('contact.html')
 
-
-def index(request):
->>>>>>> 4f8901bc6351d3b4e59ec9350137422d8a57b680
-	return render_to_response('index.html')
-
-#==============================================================================
-def listar_cursos(request):
-    #funcion que lista los cursos activos cuando el estado es 1
-	cursos = Curso.objects.filter(estado='1')
-	return render(request, 'listar_cursos.html', { 'cursos': cursos })
-
-
-def send_course(request):
-    id_course = request.GET.get('id_course')
-    name_course = request.GET.get('name_course')
-    return render(request, 'inscripcion.html', {'id_course': id_course, 'name_course': name_course})
-    #html = "<html><body><h1>id course:</h1><h3>%s<h/3> <h1> Name curso</h1><h2>%s<h/2></body></html>" % (id_course, name_course)
-    #return HttpResponse(html)
-
-
-def inscripcion(request):
-    return render_to_response('inscripcion.html')
-
-
-def registre(request):
-    errors = []
-    if request.method == 'POST':
-        if not request.POST.get('identificacion', ''):
-            errors.append('Por favor introduce la identificacion.')
-        if not errors:
-            values = [request.POST['identificacion'], request.POST['date_birth'],request.POST['sexo'],request.POST['caracter']]
-            html = "<html><body><h1></h1><h3>conas %s</h3> </body></html>" % values[0]
-            return HttpResponse(html)
-    return render(request, 'inscripcion.html', {'errors': errors,'identificacion': request.POST.get('identificacion', ''),})
-
+def information_page_view(request):
+	return render_to_response('information.html')
