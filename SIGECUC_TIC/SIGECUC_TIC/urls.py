c5e4a030918dae6ce6324414906850066a39bc4a
Don Aurelio <aurelio.vivas@correounivalle.edu.co>
2015-05-05 23:50:24 -0500

    Problema URLS solucionado

--- SIGECUC_TIC/SIGECUC_TIC/urls.py
+++ SIGECUC_TIC/SIGECUC_TIC/urls.py
@@ -2,35 +2,35 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 
-urlpatterns = patterns('aplicacion_cursos_tic.views',
+urlpatterns = patterns('',
 	# Examples:
 	# url(r'^$', 'SIGECUC_TIC.views.home', name='home'),
 	# url(r'^blog/', include('blog.urls')),
 
 	
 	# URLS INICIO DE DE SESION  
-	url(r'^$', 'pagina_principal'),
+	url(r'^$', 'aplicacion_cursos_tic.views.pagina_principal'),
 	url(r'^admin', include( admin.site.urls )),
-	url(r'^inicio', 'pagina_principal'),
-	url(r'^login', 'pagina_iniciar_sesion'),
-	url(r'^perfil', 'pagina_perfil'),
+	url(r'^inicio', 'aplicacion_cursos_tic.views.pagina_principal'),
+	url(r'^login', 'aplicacion_cursos_tic.views.pagina_iniciar_sesion'),
+	url(r'^perfil', 'aplicacion_cursos_tic.views.pagina_perfil'),
 	url(r'^cerrar_sesion','django.contrib.auth.views.logout',{'next_page':'/'}),
-	url(r'^informacion', 'pagina_informacion'),
+	url(r'^informacion', 'aplicacion_cursos_tic.views.pagina_informacion'),
 
 
 	url(r'^inscripcion', 'aplicacion_cursos_tic.views.pagina_inscripcion_persona'),
 
 	#URLS MASTER TEACHER
-	url(r'^master_teacher_informacion_personal', 'pagina_master_teacher_informacion_personal'),
-	url(r'^master_teacher_actividades_evaluacion', 'pagina_master_teacher_actividades_evaluacion'),
-	url(r'^master_teacher_estudiantes', 'pagina_master_teacher_estudiantes'),
+	url(r'^master_teacher_informacion_personal', 'aplicacion_cursos_tic.views.pagina_master_teacher_informacion_personal'),
+	url(r'^master_teacher_actividades_evaluacion', 'aplicacion_cursos_tic.views.pagina_master_teacher_actividades_evaluacion'),
+	url(r'^master_teacher_estudiantes', 'aplicacion_cursos_tic.views.pagina_master_teacher_estudiantes'),
 
 	#URSL LEADER TEACHER
-	url(r'^leader_teacher_informacion_personal', 'pagina_leader_teacher_informacion_personal'),
-	url(r'^leader_teacher_informacion_curso', 'pagina_leader_teacher_informacion_curso'),
-	url(r'^leader_teacher_calificaciones', 'pagina_leader_teacher_calificaciones'),
-	url(r'^leader_teacher_historial_academico', 'pagina_leader_teacher_historial_academico'),
-	url(r'^leader_teacher_certificados_obtenidos', 'pagina_leader_teacher_certificados_obtenidos'),
+	url(r'^leader_teacher_informacion_personal', 'aplicacion_cursos_tic.views.pagina_leader_teacher_informacion_personal'),
+	url(r'^leader_teacher_informacion_curso', 'aplicacion_cursos_tic.views.pagina_leader_teacher_informacion_curso'),
+	url(r'^leader_teacher_calificaciones', 'aplicacion_cursos_tic.views.pagina_leader_teacher_calificaciones'),
+	url(r'^leader_teacher_historial_academico', 'aplicacion_cursos_tic.views.pagina_leader_teacher_historial_academico'),
+	url(r'^leader_teacher_certificados_obtenidos', 'aplicacion_cursos_tic.views.pagina_leader_teacher_certificados_obtenidos'),
 	
 
 	#url(r'^inscripcion', 'aplicacion_cursos_tic.views.enviar_curso',name='env_curso'),
