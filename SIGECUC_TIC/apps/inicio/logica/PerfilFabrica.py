d9e5993efd4c7d413a41033fcbd5562f84d99b91
Don Aurelio <aurelio.vivas@correounivalle.edu.co>
2015-05-08 10:35:45 -0500

    Implementacion patron de diseño fabrica, usando para mostrar las paginas principales de usuarios

---
+++ SIGECUC_TIC/apps/inicio/logica/PerfilFabrica.py
@@ -0,0 +1,112 @@
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+
+from apps.inicio.models import MasterTeacher
+from apps.inicio.models import LeaderTeacher
+
+class PerfilHtml:
+
+	def __init__(self,request):
+		self.request = request
+		self.user = request.user
+
+	def obtener_pagina(self):
+		pass
+
+class AdministradorHtml(PerfilHtml):
+
+	def __init__(self,request):
+		PerfilHtml.__init__(self,request)
+
+	def obtener_pagina(self):
+		return HttpResponseRedirect('admin')
+
+class MasterTeacherHtml(PerfilHtml):
+
+	def __init__(self,request):
+		PerfilHtml.__init__(self,request)
+
+	def obtener_pagina(self):
+		return render_to_response('master_teacher.html',{'user':self.user})
+
+class LeaderTeacherHtml(PerfilHtml):
+
+	def __init__(self,request):
+		PerfilHtml.__init__(self,request)
+
+	def obtener_pagina(self):
+		return render_to_response('leader_teacher.html',{'user':self.user})
+
+class ErrorHtml(PerfilHtml):
+	def __init__(self,request,message):
+		PerfilHtml.__init__(self,request)
+		self.message = message
+
+	def obtener_pagina(self):
+		return render_to_response('login.html',{'message':self.message},context_instance=RequestContext(self.request))
+
+
+#Fabrica de paginas de usuario
+#Determina y crea la pagina o respuesta http dependiendo 
+#del usuario que esta logeado
+class FabricaPaginaPrincipalUsuario:
+
+	def __init__(self,request,formulario_login):
+		self.request = request
+		self.formulario = formulario_login
+		
+	def obtener_pagina_html_usuario(self):
+		message = ""
+		if self.formulario.is_valid():
+			username = self.request.POST['username']
+			password = self.request.POST['password']
+			user = authenticate(username=username,password=password)
+			if user is not None:
+				if user.is_active:
+					login(self.request,user)
+
+					#Si el usuario tiene permisos de Administrador 
+					#Entonces se redirecciona a la url admin
+					if user.is_staff:
+						pagina_administrador = AdministradorHtml(self.request)
+						return pagina_administrador
+
+					#Si el usuario es Master Teacher o Leader Teacher
+					tipo_MasterTeacher = 0 
+					tipo_LeaderTeacher = 0
+					user_id = user.id
+
+					#Se verifica que tipo de usuario es 
+					try:
+						master_teacher = MasterTeacher.objects.get(user_id=user_id) #Busca solo un objeto
+						tipo_MasterTeacher = 1 #tipo de usuario Mater Teacher
+					except ObjectDoesNotExist:
+						tipo_MasterTeacher = 0
+					try:
+						leader_teacher = LeaderTeacher.objects.get(user_id=user_id)
+						tipo_LeaderTeacher = 1 #tipo de usuario Mater Teacher
+					except ObjectDoesNotExist:
+						tipo_LeaderTeacher = 0
+					
+
+					if tipo_MasterTeacher == 1:
+						pagina_master_teacher = MasterTeacherHtml(self.request,user)				
+						return pagina_master_teacher
+					
+					elif tipo_LeaderTeacher == 1:
+						pagina_leader_teacher = LeaderTeacherHtml(self.request,user)
+						return pagina_leader_teacher
+				
+				elif tipo_MasterTeacher == 0 and tipo_LeaderTeacher == 0: 
+					message = "Tu usuario esta inactivo"
+			else:
+				message = "Nombre de usuario y/o password incorrecto"
+		else:
+			message = "Formulario no valido" 
+	
+		
+		error_html = ErrorHtml(self.request,message)
+		return error_html
+		
--- SIGECUC_TIC/apps/inicio/url.py
+++ SIGECUC_TIC/apps/inicio/url.py
@@ -1,15 +1,20 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+
 urlpatterns = patterns('',
 	# Examples:
 	# url(r'^$', 'SIGECUC_TIC.views.home', name='home'),
 	# URLS INICIO DE DE SESION  
 	
+	#url(r'^principal',PaginaPrincipalDetailView.as_view(),name="principal"),
+	#rl(r'^principal/(?P<slug>[-\w])/',PaginaPrincipalDetailView.as_view(),name="principal"),
+	
+	url(r'^$','apps.inicio.views.pagina_principal',name="principal"),
 	url(r'^principal','apps.inicio.views.pagina_principal',name="principal"),
 	url(r'^login','apps.inicio.views.pagina_iniciar_sesion',name="login"),
 	url(r'^perfil','apps.inicio.views.pagina_perfil',name="perfil"),
-	url(r'^cerrarsesion','django.contrib.auth.views.logout',{'next_page':'/'}),
+	url(r'^cerrarsesion','django.contrib.auth.views.logout',{'next_page':'/'}, name="cerrarsesion"),
 	url(r'^informacion','apps.inicio.views.pagina_informacion',name="informacion"),
 	
 
