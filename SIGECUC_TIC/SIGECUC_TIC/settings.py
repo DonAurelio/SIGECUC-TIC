35987b93608f6f92e6c67f197e44a51b2734fb24
EduardoSP <eduardo.saavedra@correounivalle.edu.co>
2015-05-13 21:23:43 -0500

    cambiando de idioma en el settings

--- SIGECUC_TIC/SIGECUC_TIC/settings.py
+++ SIGECUC_TIC/SIGECUC_TIC/settings.py
@@ -100,7 +100,7 @@ TEMPLEATE_PATH_INSCRIPCION,
 )
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
