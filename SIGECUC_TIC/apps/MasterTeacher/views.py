6396b54240f2365851f6602a4b49c8bc7eea4fb4
EduardoSP <eduardo.saavedra@correounivalle.edu.co>
2015-05-17 22:26:57 -0500

    Calificaciones de Master Teacher

--- SIGECUC_TIC/apps/MasterTeacher/views.py
+++ SIGECUC_TIC/apps/MasterTeacher/views.py
@@ -9,6 +9,7 @@ from apps.cursos.forms import Informacion_personalForm
 from apps.cursos.models import MasterTeacher
 from apps.cursos.models import Cohorte
 from apps.cursos.models import Curso
+from apps.cursos.models import LeaderTeacher
 
 
 
@@ -51,9 +52,13 @@ def pagina_master_teacher_actividades_evaluacion(request):
 	id_cohorte = request.GET.get('id_cohorte')
 	cohorte = Cohorte.objects.get(id=id_cohorte)
 	curso_id=cohorte.curso.id
+	#se consulta el curso
 	curso = Curso.objects.get(id=curso_id)
-	#estudiantes = LeaderTeacher_Cohorte.objects.filter(cohorte_id=id_cohorte)
-	contexto = {'cohorte': cohorte, 'curso': curso}
+	#consulta los Leader Teacher pertenecientes a la cohorte 
+	estudiantes = LeaderTeacher.objects.filter(cohorte__id=id_cohorte)
+	actividad_evaluacion = curso.nombre
+
+	contexto = {'cohorte': cohorte, 'curso': curso, 'estudiantes': estudiantes}
 	return render_to_response('master_teacher_actividades_de_evaluacion.html',contexto, context_instance= RequestContext(request))
 	#html = "<html><body><h1>id cohorte:</h1><h3>%s<h/3> <h1> Name curso</h1><h2><h/2></body></html>" % curso
 	#return HttpResponse(html)
